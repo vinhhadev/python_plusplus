01474ebb8f8bb5f455f90240db6d59f0bb3c83ef
Vinh Ha <vinhha.dev@gmail.com>
2021-09-11 23:01:38 +0700

    first commit

---
+++ exercise01.py
@@ -0,0 +1,12 @@
+# 5 biến đúng quy tắc
+number_01 = 1
+number_02 = 3.14
+current_year = 2021
+subject = 'Python'
+days_of_week = ['Monday', 'Tuesday', 'Wednesday', 'Thurday', 'Friday', 'Satuday']
+
+print(number_01)
+print(number_02)
+print(f'This year: {current_year}')
+print(subject)
+print(days_of_week)
